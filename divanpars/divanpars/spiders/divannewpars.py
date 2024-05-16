28806ac4237ed358b1314f0c88cbd500f313b49a
PavelStarun <starunpavelpython@gmail.com>
2024-05-16 14:37:22 +0300

    добавил д\з по PS05

---
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -0,0 +1,18 @@
+import scrapy
+
+
+class DivannewparsSpider(scrapy.Spider):
+    name = "divannewpars"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
+
+    def parse(self, response):
+        lights = response.css("div._Ud0k")
+
+        for light in lights:
+            yield {
+                "name": light.css("div.lsooF span::text").get(),
+                "price": light.css("div.pY3d2 span::text").get(),
+                "url": light.css("a").attrib["href"],
+            }
+
